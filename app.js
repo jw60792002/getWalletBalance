63c5128441103a19643e9300f857bb6681913503
Jack Wong <jackwong@Jacks-MacBook-Air-2.local>
2024-08-25 20:29:41 +0800

    Remove debug log

--- app.js
+++ app.js
@@ -39,13 +39,10 @@ async function initWeb3(infuraProjectId) {
 app.post('/wallet-info', async (req, res) => {
   const { chain, walletAddress } = req.body;
   const infuraUrl = `https://mainnet.infura.io/v3/${infuraProjectId}`;
-  console.log(infuraUrl);
   try {
     if (chain === 'ethereum') {
       const { web3, provider } = await initWeb3(infuraProjectId);
       const balances = await getEthereumWalletInfo(web3, provider, walletAddress);
-      console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--')
-      console.log(balances)
       res.json({
         balances
         //infuraUrl: infuraUrl // Add INFURA_URL to the response
@@ -63,17 +60,11 @@ app.post('/wallet-info', async (req, res) => {
 // Function to get Ethereum wallet info
 async function getEthereumWalletInfo(web3, provider, walletAddress) {
   const ethBalance = await web3.eth.getBalance(walletAddress);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--1')
   const usdcBalance = await getTokenBalance(web3, USDC_ADDRESS, walletAddress);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--2')
   const usdtBalance = await getTokenBalance(web3, USDT_ADDRESS, walletAddress);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--3')
   const ethVolume = await getTransferVolume(provider, null, walletAddress, web3);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--4')
   const usdcVolume = await getTransferVolume(provider, USDC_ADDRESS, walletAddress, web3);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--5')
   const usdtVolume = await getTransferVolume(provider, USDT_ADDRESS, walletAddress, web3);
-  console.log('-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--6')
   return {
     balances: {
       ETH: web3.utils.fromWei(ethBalance, 'ether'),
@@ -101,10 +92,8 @@ async function getTokenBalance(web3, tokenAddress, walletAddress) {
 async function getTransferVolume(provider, tokenAddress, walletAddress, web3) {
   const latestBlock = await provider.getBlockNumber();
   const fromBlock = latestBlock - 7200; // 7200, Last ~24 hours of blocks
-  console.log('getTransferVolume:' + fromBlock);
   let events;
   if (tokenAddress) {
-    console.log('getTransferVolume22:' + fromBlock);
     const contract = new ethers.Contract(tokenAddress, ERC20_ABI, provider);
     const transferFilter = {
       address: contract.address,
@@ -122,20 +111,17 @@ async function getTransferVolume(provider, tokenAddress, walletAddress, web3) {
 
   // Initialize the volume to zero
   let volume = ethers.BigNumber.from(0);
-  console.log(`Processing ${events.length} events:`);
+
   // Safely process the events
   for (const event of events) {
-    console.log('Event:', event); // Log entire event object
 
     // Convert the HEX data to BigNumber
     const valueHex = event.data; // The HEX format data from the event
     const value = ethers.BigNumber.from(valueHex); // Convert HEX to BigNumber
 
     // Log the value before addition
-    console.log(`Event value before addition: ${ethers.utils.formatUnits(value, tokenAddress ? 6 : 'ether')}`);
     volume = volume.add(value);
   }
-  console.log(`Total volume: ${ethers.utils.formatUnits(volume, tokenAddress ? 6 : 'ether')}`);
   return ethers.utils.formatUnits(volume, tokenAddress ? 6 : 'ether');
 }
 
@@ -143,15 +129,12 @@ async function getTransferVolume(provider, tokenAddress, walletAddress, web3) {
 // Function to get ETH transfers
 async function getEthTransfers(web3, provider, address, fromBlock, toBlock) {
 
-  console.log("From Block:", fromBlock);
-  console.log("To Block:", toBlock);
   const transfers = [];
   {
     const batchSize = 100; // Adjust this value based on the API rate limits
 
     // Fetch transfers in batches to avoid hitting API limits
     for (let i = fromBlock; i <= toBlock; i += batchSize) {
-      console.log('iiiiiiiiii:' + i);
       const endBlock = Math.min(i + batchSize - 1, toBlock);
       const batchTransfers = await provider.send("eth_getLogs", [{
         fromBlock: web3.utils.toHex(i),
